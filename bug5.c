fbc72b780fefd26410691c3f93e03a85e0297d0f
Buganini Q <buganini@gmail.com>
2011-04-23 18:19:12 +0800

    remove freebsd-specific code and add my copyright

--- bug5.c
+++ bug5.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2011 buganini@gmail.com . All rights reserved.
  * Copyright (c) 1980, 1992, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -26,20 +27,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
-#include <sys/cdefs.h>
-
-__FBSDID("$FreeBSD$");
-
-#ifndef lint
-static const char copyright[] =
-"@(#) Copyright (c) 1980, 1992, 1993\n\
-	The Regents of the University of California.  All rights reserved.\n";
-#endif
-
-#ifndef lint
-static const char sccsid[] = "@(#)script.c	8.1 (Berkeley) 6/6/93";
-#endif
 
 #include <sys/types.h>
 #include <sys/wait.h>
